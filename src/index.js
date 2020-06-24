03b43b2d8d09c2d04b853a2a723eaf4558d06c8f
Ash Cole <45434172+ash-sp-cole@users.noreply.github.com>
2020-06-24 17:51:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+impor
+t App from './App';
 ReactDOM.render(
 
+    
     <App />
+    
   ,
   document.getElementById('root')
 );
